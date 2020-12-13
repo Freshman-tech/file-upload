4860c265d133a3f71731841537bf654b7a66d579
dadez <dadez@pm.me>
2020-12-13 16:44:15 +0100

    use logger, write file with is original name, add some debug log infos

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/freshman-tech/file-upload
 
 go 1.15
+
+require github.com/sirupsen/logrus v1.7.0
--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
-	"path/filepath"
-	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 const MAX_UPLOAD_SIZE = 1024 * 1024 // 1MB
@@ -18,6 +17,19 @@ const MAX_UPLOAD_SIZE = 1024 * 1024 // 1MB
 type Progress struct {
 	TotalSize int64
 	BytesRead int64
+}
+
+// configure logging
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
 }
 
 // Write is used to satisfy the io.Writer interface.
@@ -40,12 +52,14 @@ func (pr *Progress) Print() {
 	fmt.Printf("File upload in progress: %d\n", pr.BytesRead)
 }
 
+// IndexHandler serves the index.html file
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 	http.ServeFile(w, r, "index.html")
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	log.Debug("method is: ", r.Method)
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -59,8 +73,10 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	// get a reference to the fileHeaders
 	files := r.MultipartForm.File["file"]
+	log.Debug("files are", files)
 
 	for _, fileHeader := range files {
+		log.Debug("uploading file ", fileHeader.Filename)
 		if fileHeader.Size > MAX_UPLOAD_SIZE {
 			http.Error(w, fmt.Sprintf("The uploaded image is too big: %s. Please use an image less than 1MB in size", fileHeader.Filename), http.StatusBadRequest)
 			return
@@ -82,6 +98,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		filetype := http.DetectContentType(buff)
+		log.Debug("File type is: ", filetype)
 		if filetype != "image/jpeg" && filetype != "image/png" && filetype != "application/pdf" {
 			http.Error(w, "The provided file format is not allowed. Please upload a JPEG, PNG or PDF file", http.StatusBadRequest)
 			return
@@ -93,13 +110,15 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		log.Debug("creating upload directory")
 		err = os.MkdirAll("./uploads", os.ModePerm)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		f, err := os.Create(fmt.Sprintf("./uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
+		//f, err := os.Create(fmt.Sprintf("./uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
+		f, err := os.Create(fmt.Sprintf("./uploads/%s", fileHeader.Filename))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
